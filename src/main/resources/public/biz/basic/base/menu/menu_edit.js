a29ec7ccfe2477e5aef33a82c95a5395563e59fa
刘小平 <contact@liuxp.me>
2020-12-31 12:45:22 +0800

    菜单管理：修改时支持选择父级菜单。

--- src/main/resources/public/biz/basic/base/menu/menu_edit.js
+++ src/main/resources/public/biz/basic/base/menu/menu_edit.js
@@ -1,3 +1,39 @@
+var page = {
+    // 方法集合
+    fn:{
+        loadParent : function (menuLevel,id) {
+            if(menuLevel == 0){
+                // 取得一级菜单的父级，默认为0
+                var options = "<option value=\"0\">默认根节点</option>";
+                $("#menuParentId").html(options);
+            }else{
+                // 取得二、三、四、五级菜单的父级
+                $.alicej.util.ajax({
+                    type: "POST",
+                    url: "/web/basic/base/menu/selectList",
+                    data : {
+                        menuLevel : menuLevel - 1
+                    },
+                    dataType:'json',
+                    success: function(rsp){
+                        var options = "<option value=\"\" selected=\"selected\">请选择</option>";
+                        if(rsp){
+                            for(var i=0;i<rsp.length;i++){
+                                if(undefined!==id && id == rsp[i].id){
+                                    options += "<option value=\""+rsp[i].id+"\" selected='selected' >"+rsp[i].menuName+"</option>";
+                                }else{
+                                    options += "<option value=\""+rsp[i].id+"\">"+rsp[i].menuName+"</option>";
+                                }
+                            }
+                        }
+                        $("#menuParentId").html(options);
+                    }
+                });
+            }
+        }
+    }
+};
+
 $(function () {
 
     // 取得待修改的数据
@@ -13,7 +49,6 @@ $(function () {
             $("#menuDesc").val(rsp.menuDesc);
             $("#menuUrl").val(rsp.menuUrl);
             $("#menuIcon").val(rsp.menuIcon);
-            $("#menuParentId").val(rsp.menuParentId);
             $("#menuLevel").val(rsp.menuLevel);
             $("#menuType").val(rsp.menuType);
             $("#remark").val(rsp.remark);
@@ -45,6 +80,8 @@ $(function () {
                 placeholder: '请选择',
                 minimumResultsForSearch: Infinity // 隐藏搜索框
             });
+
+            page.fn.loadParent(rsp.menuLevel,rsp.menuParentId);
         }
     });
 
@@ -66,5 +103,9 @@ $(function () {
                 });
             }
         });
+    });
+
+    $("#menuLevel").change(function () {
+        page.fn.loadParent($(this).val());
     });
 });
