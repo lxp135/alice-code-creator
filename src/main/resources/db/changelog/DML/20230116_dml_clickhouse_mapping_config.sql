916601e12ae0fe6def03a6c8e44f50a800f27b21
刘小平 <contact@liuxp.me>
2023-01-16 15:05:08 +0800

    添加clickhouse数据库支持，数据库脚本。

---
+++ src/main/resources/db/changelog/DML/20230116_dml_clickhouse_mapping_config.sql
@@ -0,0 +1,6 @@
+-- liquibase formatted sql
+-- changeset contact@liuxp.me:20230116_dml_clickhouse_mapping_config
+
+INSERT INTO `generator_config_mapping` VALUES (49, 'ClickHouse', 'String', 'VARCHAR', 'String', '2023-01-16 14:39:19', '刘小平', '2023-01-16 14:39:19', '刘小平', 0);
+INSERT INTO `generator_config_mapping` VALUES (50, 'ClickHouse', 'Decimal', 'DECIMAL', 'BigDecimal', '2023-01-16 14:40:26', '刘小平', '2023-01-16 14:40:26', '刘小平', 0);
+INSERT INTO `generator_config_mapping` VALUES (51, 'ClickHouse', 'DateTime', 'TIMESTAMP', 'Date', '2023-01-16 14:41:36', '刘小平', '2023-01-16 14:41:36', '刘小平', 0);
